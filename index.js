767df3a523d656b575e886e41f344ef45dba131c
Mike Szörnyi <mike.szoernyi@gmail.com>
2017-08-03 21:58:50 +0200

    (refactor) avoid name mapping

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const noGetThis = require('./lib/rules/no-get-this');
-const computedSyntax = require('./lib/rules/ember-array-dependency-syntax');
+const emberArrayDependencySyntax = require('./lib/rules/ember-array-dependency-syntax');
 
 module.exports.rules = {
   "no-get-this": noGetThis,
-  "ember-array-dependency-syntax": computedSyntax,
+  "ember-array-dependency-syntax": emberArrayDependencySyntax,
 };
